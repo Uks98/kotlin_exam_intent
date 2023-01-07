80bae72da6c1d6c658a5f7f38be96811717962f2
uks98 <rozyfactory@gmail.com>
2023-01-07 23:45:44 +0900

    리사이클러 뷰 연습

---
+++ app/src/main/java/com/example/practice_day3/Memo.kt
@@ -0,0 +1,3 @@
+package com.example.practice_day3
+
+data class Memo(var no : Int, var title : String, var timeStamp : Long)
--- app/src/main/java/com/example/practice_day3/RecycleViewPage.kt
+++ app/src/main/java/com/example/practice_day3/RecycleViewPage.kt
@@ -2,6 +2,7 @@ package com.example.practice_day3
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.practice_day3.databinding.ActivityRecycleViewPageBinding
 
 class RecycleViewPage : AppCompatActivity() {
@@ -11,5 +12,23 @@ class RecycleViewPage : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+        fun longData () : MutableList<Memo> {
+            val data : MutableList<Memo> = mutableListOf()
+            for(no in 1 .. 100){
+                val title = "이것이 안드로이드다 ${no}"
+                val date = System.currentTimeMillis()
+                var memo = Memo(no,title,date)
+                data.add(memo)
+            }
+            return data
+        }
+        val data : MutableList<Memo> = longData()
+
+        var adapter = CustomAdapter()
+        adapter.listData = data
+
+        binding.recycleView.adapter = adapter
+        binding.recycleView.layoutManager = LinearLayoutManager(this)
+
     }
 }
